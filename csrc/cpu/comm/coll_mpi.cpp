cb9f6799079a1d341f83efe73a8475ab240a61a6
Ma, Guokai <guokai.ma@intel.com>
2023-10-17 22:31:31 -0400

    don't init mpi since this is already initialized in oneCCL

--- csrc/cpu/comm/coll_mpi.cpp
+++ csrc/cpu/comm/coll_mpi.cpp
@@ -3,11 +3,11 @@
 
 void init_mpi(void)
 {
-    MPI_Init(NULL, NULL);
+    //MPI_Init(NULL, NULL);
 
-    int size, rank;
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    //int size, rank;
+    //MPI_Comm_size(MPI_COMM_WORLD, &size);
+    //MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 }
 
 void mpi_all_reduce(int world_size, int rank, void* buf, size_t data_size, size_t numel, c10::ScalarType scalar_type)
